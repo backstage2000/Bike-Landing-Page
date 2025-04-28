5ada1654f6b525bc4870f4d7b8bab94bca863f07
Anatoliy Korotenko <tolik.korotenko@21gmail.com>
2025-04-28 12:32:57 +0300

    add page

---
+++ src/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const burger = document.querySelector('.icon--menu');
+const close = document.querySelector('.icon--close');
+const menu = document.querySelector('.menu');
+
+burger.addEventListener('click', () => {
+  menu.classList.add('active');
+  document.body.classList.add('disable-scroll');
+});
+
+close.addEventListener('click', () => {
+  menu.classList.remove('active');
+  document.body.classList.remove('disable-scroll');
+});
+
+document
+  .querySelector('.contact__button')
+  .addEventListener('click', function(event) {
+    event.preventDefault();
+
+    document.querySelector('.contact__form').reset();
+  });
