5601aa4c9648cad76f5203ef3ef5351be6a3c6ba
Anatoliy Korotenko <tolik.korotenko@21gmail.com>
2025-04-29 11:42:55 +0300

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: '@mate-academy/eslint-config',
+  extends: "@mate-academy/eslint-config",
+  env: {
+    browser: true,
+    es2021: true,
+  },
 };
--- src/main.js
+++ src/main.js
@@ -1,23 +1,29 @@
-'use strict';
+"use strict";
 
-const burger = document.querySelector('.icon--menu');
-const close = document.querySelector('.icon--close');
-const menu = document.querySelector('.menu');
+const burger = document.querySelector(".icon--menu");
+const handClose = document.querySelector(".icon--close");
+const menu = document.querySelector(".menu");
 
-burger.addEventListener('click', () => {
-  menu.classList.add('active');
-  document.body.classList.add('disable-scroll');
+burger.addEventListener("click", () => {
+  menu.classList.add("active");
+  document.body.classList.add("disable-scroll");
 });
 
-close.addEventListener('click', () => {
-  menu.classList.remove('active');
-  document.body.classList.remove('disable-scroll');
+handClose.addEventListener("click", () => {
+  menu.classList.remove("active");
+  document.body.classList.remove("disable-scroll");
 });
 
 document
-  .querySelector('.contact__button')
-  .addEventListener('click', function(event) {
-    event.preventDefault();
+  .querySelector(".contact__form")
+  .addEventListener("submit", function (e) {
+    if (!this.checkValidity()) {
+      return;
+    }
 
-    document.querySelector('.contact__form').reset();
+    e.preventDefault();
+
+    alert("Form successfully submitted!");
+
+    this.reset();
   });
